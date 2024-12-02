2e143c9e072ad7d711b9e03d238e9ca9a2c13d17
Miguel Quenta <angeloquenta@gmail.com>
2024-12-02 09:02:29 -0400

    fixing to close test

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Hello, Jenkins!');
 });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (require.main === module){
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
 
 module.exports = app;
